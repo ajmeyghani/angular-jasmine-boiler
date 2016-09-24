8160a53d51b37707d2aae098353e000819c26f55
Amin Meyghani <meyghania@gmail.com>
2016-09-23 19:57:27 -0700

    add readme, clean tests.

--- main-ctrl.test.js
+++ main-ctrl.test.js
@@ -1,29 +1,34 @@
 describe('Main Controller', function() {
-  var underTest, $scope;
+  var underTest, scp;
   beforeEach(function() {
+    /* run the following before each describe block */
     angular.mock.module('app');
     angular.mock.inject(function($controller, $rootScope) {
-      $scope = $rootScope.$new();
+      scp = $rootScope.$new();
       underTest = $controller('mainCtrl', {
-        $scope: $scope
+        $scope: scp
       });
     });
   });
+  /* From each describe block to another, the controller and the 
+  states of scope are refreshed.
+  */
   describe('The add method', function() {
     it('should add an item to $scope.app.items when called with an object', function() {
       underTest.add({id: 1, name: 'boo'});
       underTest.add({id: 2, name: 'boooo'});
-      expect($scope.app.items.length).toBe(2);
+      expect(scp.app.items.length).toBe(2);
     });
   });
   describe('The remove method', function() {
-    it('should remove an item from $scope.app.items when called with an object', function() {
-      $scope.app.items = [
+    it('should remove an item from scp.app.items when called with an object', function() {
+      // set initial state of the items.
+      scp.app.items = [
         {id: 1}, {id: 2}, {id: 3}
       ];
       underTest.remove({id: 3});
-      expect($scope.app.items.length).toBe(2);
-      expect($scope.app.items).toEqual([{id: 1}, {id: 2}]);
+      expect(scp.app.items.length).toBe(2);
+      expect(scp.app.items).toEqual([{id: 1}, {id: 2}]);
     });
   });
 });
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Angular Jasmine Boiler
+
+Boilerplate for writing tests for Angular with Jasmine.
+
+## Getting Started
+
+> Make sure you have Node installed
+
+- First, install bower with `npm i bower -g`
+- Then, inside the root of the project run `bower install` to install the dependencies.
+
+Now that you have all the dependencies, we can server the project with a server. You can use `http-server .` to server the project. If you don't have `http-server` installed, you can install it with `npm i http-server -g`
+
+Once the server has started, you can then navigate to `run-tests.html` to see the tests.
